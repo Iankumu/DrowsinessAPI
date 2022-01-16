66d1d1d7f7022a2fb923ad02360807cc81c80ed6
IanKumu <ikariuki741@gmail.com>
2022-01-16 16:55:56 +0300

    changes

--- app/Models/Drowsiness.php
+++ app/Models/Drowsiness.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use App\Models\User;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 class Drowsiness extends Model
 {
     use HasFactory;
+
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d');
+    }
 
     protected $table = 'drowsiness';
 
